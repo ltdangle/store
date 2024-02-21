044dc0874100043e1c52c438d9015f5189af5bd6
Evgeniy Fedorov <mailbox@z01d.net>
2024-02-21 12:17:07 +0200

    wip

--- pkg/models/cart.go
+++ pkg/models/cart.go
@@ -10,11 +10,11 @@ type Cart struct {
 	ID        uint   `gorm:"primarykey"`
 	Uuid      string `gorm:"unique"`
 	UserUuid  string
-	CartItems []*CartItem    `gorm:"foreignKey:CartID"`
-	Total     int            `gorm:"not null;default:0"`
-	CreatedAt time.Time      `schema:"-"`
-	UpdatedAt time.Time      `schema:"-"`
-	DeletedAt gorm.DeletedAt `schema:"-"`
+	CartItems []*CartItem `gorm:"foreignKey:CartID"`
+	Total     int         `gorm:"not null;default:0"`
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	DeletedAt gorm.DeletedAt
 }
 
 func NewCart() *Cart {
