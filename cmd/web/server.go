8fad6e395c08eaf6d6f565afb9c49b373d19854b
Evgeniy Fedorov <mailbox@z01d.net>
2024-02-21 12:58:57 +0200

    loggedRouter

--- cmd/web/server.go
+++ cmd/web/server.go
@@ -20,8 +20,10 @@ func main() {
 	dc.Router.HandleFunc("/cartItem/{uuid}/edit", dc.CartController.EditCart).Name(web.CART_EDIT_ROUTE)
 	dc.Router.HandleFunc("/seed", seed).Methods("GET")
 
+	loggedRouter := logAllResponsesMiddleware(dc.Router)
+
 	log.Info("Starting server on localhost:8080")
-	err := http.ListenAndServe("localhost:8080", dc.Router)
+	err := http.ListenAndServe("localhost:8080", loggedRouter)
 	if err != nil {
 		log.Fatalf(err.Error())
 	}
@@ -49,6 +51,29 @@ func seed(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, out.String())
 }
 
+func logAllResponsesMiddleware(handler http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		rec := newStatusRecorder(w)
+		handler.ServeHTTP(rec, r)
+
+		// Now you can log the response status
+		log.Printf("%s %s %d", r.Method, r.RequestURI, rec.status)
+	})
+}
+
+func newStatusRecorder(w http.ResponseWriter) *responseRecorder {
+	return &responseRecorder{w, http.StatusOK}
+}
+
+type responseRecorder struct {
+	http.ResponseWriter
+	status int
+}
+
+func (rec *responseRecorder) WriteHeader(code int) {
+	rec.status = code
+	rec.ResponseWriter.WriteHeader(code)
+}
 func loggingMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		// Do stuff here
