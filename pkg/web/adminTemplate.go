ad6150ddea067f605e4cab47a889bc6834ce2ae2
Evgeniy Fedorov <mailbox@z01d.net>
2024-03-21 15:32:01 +0200

    removed *.html templates

--- pkg/web/adminTemplate.go
+++ pkg/web/adminTemplate.go
@@ -3,13 +3,7 @@ package web
 import (
 	"bytes"
 	"context"
-	"log"
-	"os"
-	"path/filepath"
-	"runtime"
 	"store/pkg/repo"
-	"strconv"
-	"strings"
 )
 
 type Link struct {
@@ -52,45 +46,12 @@ type VM struct {
 }
 
 func (t *AdminTmpl) cart(cartVM *repo.CartVM) string {
-	html := LoadTemplate("cart.html")
-	var cartItems string
 
-	for _, item := range cartVM.CartItems {
-		cartItems += t.cartItem(item) + "\n"
+	var h bytes.Buffer
+	templ := cart(cartVM, t.router)
+	_ = templ.Render(context.Background(), &h)
 
-	}
-	html = strings.Replace(html, "###cart_items###", cartItems, -1)
-	html = strings.Replace(html, "###edit_link###", t.router.UrlInternal(ADMIN_VIEW_ENTITY_ROUTE, "entity", "cart", "uuid", cartVM.Cart.Uuid), -1)
-	return html
-}
-
-func (t *AdminTmpl) cartItem(item repo.CartItemVM) string {
-	html := LoadTemplate("cart_item.html")
-	html = strings.Replace(html, "###name###", item.Product.Name, -1)
-	html = strings.Replace(html, "###description###", item.Product.Description, -1)
-	html = strings.Replace(html, "###price###", "$ "+strconv.Itoa(item.CartItem.Subtotal), -1)
-	var fields []string
-	html = strings.Replace(html, "###product_fields###", strings.Join(fields, "\n"), -1)
-	html = strings.Replace(html, "###remove_link###", t.router.UrlInternal(CART_ITEM_DELETE_ROUTE, "uuid", item.CartItem.Uuid), -1)
-	return html
-}
-
-// TODO: use a-ha/templ
-func LoadTemplate(tmpl string) string {
-	_, currentFilePath, _, ok := runtime.Caller(0)
-	if !ok {
-		log.Fatal("No caller information")
-	}
-
-	currentDir := filepath.Dir(currentFilePath)
-
-	targetFilePath := filepath.Join(currentDir+"/tmpl/", tmpl)
-	content, err := os.ReadFile(targetFilePath)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	return string(content)
+	return h.String()
 }
 
 func (t *AdminTmpl) AddNavLink(url string, text string) {
